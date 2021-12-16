4428318969314f2d0cf020edac5d63cd39b4f410
Stephenlaye2 <32752271+Stephenlaye2@users.noreply.github.com>
2021-12-16 11:00:18 +0000

    first commit

---
+++ task1.py
@@ -0,0 +1,38 @@
+import os
+import requests
+import json
+from config import config
+
+
+Bearer_Token = config.Bearer_Token
+Twitter_Username = config.Twiiter_Username
+
+
+def url_endpoint():
+  tweet_fields = 'tweet.fields=public_metrics,created_at'
+  url = f'https://api.twitter.com/2/tweets/search/recent?max_results=100&query=from:{Twitter_Username}&{tweet_fields}'
+
+  return url
+
+def bearer_oauth(r):
+  """
+  Method for bearer token authentication.
+  """
+  r.headers['Authorization'] = f'Bearer {Bearer_Token}'
+
+  
+  return r
+
+def get_data(url):
+  response = requests.request('GET', url, auth=bearer_oauth)
+  if response.status_code != 200:
+    raise Exception(f"Status Code: {response.status_code}, Error Message: {response.text}")
+  return response.json()
+
+def main():
+  url = url_endpoint()
+  json_response = get_data(url)
+  json_data = json.dumps(json_response, indent=4, sort_keys=True)
+  print(json_data)
+
+main()
