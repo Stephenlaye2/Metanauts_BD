b2b21862cc7645a47835758e82421acc39c3917b
Stephenlaye2 <32752271+Stephenlaye2@users.noreply.github.com>
2021-12-17 19:34:24 +0000

    Dictionary of each word and number of occourence

--- Task 2/task2.py
+++ Task 2/task2.py
@@ -11,34 +11,46 @@ with open('shakespeare.txt', 'r') as shakespeare_file:
 
 
 #Use regular expression to match the words
-p = re.compile('[a-zA-Z][-_.\'a-zA-Z]*')
+p = re.compile('[a-zA-Z][-_\'a-zA-Z]*')
 
 # Find all the matched word and store them in a list
-matched_data = p.findall(data)
-print(matched_data)
+matched_data = p.findall(data.lower())
+# print(matched_data)
 
 for word in matched_data:
-  if '.' in word and len(word) == 2:
-    print(word)
-    continue
-  elif '--' in word:
-    print(word)
+  # Separate words with two dashes in between them
+  if '--' in word:
     two_dashes_words += f'{word}--'
     continue
-
+    # Append other words to clean_data
   clean_data.append(word)
 
+# Split words with two dashes by --
 new_string = two_dashes_words.replace('----', '--')
 split_dashes = new_string.split('--')
 
+# Remove the empty string from the array
+split_dashes.remove('')
+
+# Add both the array of clean_data and split_dashes
+combined_words = clean_data + split_dashes
+
 """
 FOR DEBUGING PURPOSE
+print(combined_words)
 print(len(matched_data))
 print(two_dashes_words.replace('----', '--'))
 print(split_dashes)
 print(len(split_dashes)-1)
 """
 
-word_count['word_count'] = len(clean_data) + len(split_dashes)-1
+# Count each word in the array
+word_count['Total Words'] = len(combined_words)
+joined_word = ' '.join(combined_words)
+for word in combined_words:
+  count = joined_word.count(word)
+  word_count[word] = count
+
+# print(joined_word)
 print(word_count)
 
