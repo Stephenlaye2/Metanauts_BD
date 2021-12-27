5ffa9b69932ad277df1bf6025019338293be439b
Stephenlaye2 <32752271+Stephenlaye2@users.noreply.github.com>
2021-12-27 15:08:28 +0000

    Quiz 4 programming

---
+++ Quiz 4/quiz_4.py
@@ -0,0 +1,22 @@
+# Programming 1
+nums1 = [1, 3]
+nums2 = [2]
+nums = nums1 + nums2
+nums.sort()
+print(nums)
+
+mid = int(len(nums)/2)
+median = 0
+
+median = (nums[mid] + nums[~mid])/2
+
+
+print(median)
+
+
+# Programming Question 18
+
+num1 = "2"
+num2 = "3"
+add = str(int(num1) * int(num2))
+print(add)
---
+++ Quiz 4/quiz_4.sql
@@ -0,0 +1,4 @@
+-- Question 16
+SELECT * FROM 'Stadium table'
+WHERE people = 100 OR people > 100
+ORDER BY visit_date
